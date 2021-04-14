a6d352557fb23ea72bb3de77137d11b311878294
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2021-04-14 22:19:11 +0200

    Change background color

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import Message from './Message';
+import "./App.css";
+import Message from "./Message";
 
-import { Button, FormControl, InputLabel, Input, IconButton} from '@material-ui/core';
-import FlipMove from 'react-flip-move';
-import SendIcon from '@material-ui/icons/Send';
-import Icon from '@material-ui/core/Icon';
+import { FormControl, InputLabel, Input, IconButton } from "@material-ui/core";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
 
-import db from './firebase';
-import firebase from 'firebase';
+import db from "./firebase";
+import firebase from "firebase";
 
 function App() {
-  const [input, setInput] = React.useState('');
+  const [input, setInput] = React.useState("");
   const [messages, setMessages] = React.useState([]);
-  const [username, setUsername] = React.useState('');
+  const [username, setUsername] = React.useState("");
 
   // console.log('input',input);
   // console.log('messages',messages);
@@ -22,48 +21,63 @@ function App() {
   //adding to db
   React.useEffect(() => {
     // run once when the app component loads
-    db.collection('messages').orderBy('timestamp', 'desc')
-    .onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})));
-    });
+    db.collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
+      });
   }, []);
 
   React.useEffect(() => {
-    setUsername(prompt('Enter your nickname'));
+    setUsername(prompt("Enter your nickname"));
   }, []);
 
   const sendMessage = (event) => {
     event.preventDefault(); //dont refresh page on clicking 'enter'
 
-    db.collection('messages').add({
+    db.collection("messages").add({
       data: input,
       username: username,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    })
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
 
     // setMessages([...messages, {username: username, text: input}]);
-    setInput('');
+    setInput("");
   };
 
   return (
     <div className="App">
-      <img src="https://web.telegram.org/img/logo_share.png" />
-        <h1>FastMessenger</h1>
-        <h2>Welcome {username}</h2>
-        <FlipMove>
-          {messages.map(({id,message}) => (
-            <Message key={id} username={username} message={message}/>
-          ))}
-        </FlipMove>
-        <form className="app__form">
-          <FormControl className="app__formControl">
-            <InputLabel >Enter a message...</InputLabel>
-            <Input className="app__input" placeholder='Enter a message' value={input} onChange={event => setInput(event.target.value)} />
-            <IconButton className="app__iconButton" variant="contained" color="primary" disabled={!input} type='submit' onClick={sendMessage}>
-              <SendIcon />
-            </IconButton>
-          </FormControl>
-        </form>
+      <img alt="logo" src="https://web.telegram.org/img/logo_share.png" />
+      <h1>FastMessenger</h1>
+      <h2>Welcome {username || "Anonymous"f}</h2>
+      <FlipMove>
+        {messages.map(({ id, message }) => (
+          <Message key={id} username={username} message={message} />
+        ))}
+      </FlipMove>
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <InputLabel>Enter a message...</InputLabel>
+          <Input
+            className="app__input"
+            placeholder="Enter a message"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <IconButton
+            className="app__iconButton"
+            variant="contained"
+            color="primary"
+            disabled={!input}
+            type="submit"
+            onClick={sendMessage}
+          >
+            <SendIcon />
+          </IconButton>
+        </FormControl>
+      </form>
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Card, CardContent, Typography} from '@material-ui/core';
-import './Message.css';
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./Message.css";
 
-const Message = React.forwardRef(({username, message},ref) => {
-        const isUser = username === message.username;
+const Message = React.forwardRef(({ username, message }, ref) => {
+  const isUser = username === message.username;
 
-    return (
-        <div ref={ref} className={`message ${isUser && 'message__user'}`}>
-            <Card className={isUser ? "message__userCard": "message__guestCard" }>
-                <CardContent>
-                    <Typography color="textPrimary" variant="h5" component="h2">
-                            {!isUser && `${message.username  || 'Anonymous'}: `} {message.data}
-                        </Typography>
-                </CardContent>
-            </Card>
-        </div>
-    )
+  return (
+    <div ref={ref} className={`message ${isUser && "message__user"}`}>
+      <Card className={isUser ? "message__userCard" : "message__guestCard"}>
+        <CardContent>
+          <Typography color="textPrimary" variant="h5" component="h2">
+            {!isUser && `${message.username || "Anonymous"}: `} {message.data}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 });
 
 export default Message;
